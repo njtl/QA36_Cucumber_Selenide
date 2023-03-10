0313db9ac9b0a96e5e32a40d7409844b2c960221
njtl <ilya.maximov@me.com>
2023-03-10 10:52:55 +0100

    Fixed issue with chromium111, using firefox instead. Added step to close the driver

--- src/test/java/com/telran/otto/pages/HomePage.java
+++ src/test/java/com/telran/otto/pages/HomePage.java
@@ -1,20 +1,28 @@
 package com.telran.otto.pages;
 
+import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
+
 public class HomePage {
     private static By okCookiesButton = By.cssSelector("#cookieBanner > div > div > div.cookieBanner__footer > button");
-    private static By meinKontoIcon = By.cssSelector("[data-qa='user_login_area_header_container']");
+    private static By meinKontoIcon = By.cssSelector("[data-qa='user_login_area_icon']");
+
     private static By anmeldenButton = By.cssSelector("[data-qa='user_login_area_login']");
     private static By loggedInIcon = By.cssSelector("[data-qa='user_login_area_badge_logged_in']");
     public void acceptCookies() {
         $(okCookiesButton).click();
     }
     public void goToMeinKonto() {
-        $(meinKontoIcon).click();
+        sleep(1000);
+        System.out.println("enabled "+$(meinKontoIcon).isEnabled());
+        $(meinKontoIcon).click(ClickOptions.usingJavaScript());
+        sleep(1000);
+        System.out.println("exists "+$(anmeldenButton).exists());
         $(anmeldenButton).click();
     }
     public SelenideElement verifyAuthIcon() {
--- src/test/java/com/telran/otto/steps/HomePageSteps.java
+++ src/test/java/com/telran/otto/steps/HomePageSteps.java
@@ -1,17 +1,25 @@
 package com.telran.otto.steps;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
 import com.telran.otto.pages.HomePage;
 import io.cucumber.java8.En;
+import org.junit.After;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class HomePageSteps implements En {
     HomePage homePage;
     String baseUrl = "https://www.otto.de/";
+    WebDriver driver;
+
 
     public HomePageSteps() {
         Given("that we navigate to home page", () -> {
+            driver = new FirefoxDriver();
+            WebDriverRunner.setWebDriver(driver);
             homePage = open(baseUrl, HomePage.class);
         });
 
@@ -26,5 +34,10 @@ public class HomePageSteps implements En {
         Then("user icon shows that we're logged in", () -> {
             homePage.verifyAuthIcon().shouldBe(Condition.visible);
         });
+        And("^we close the driver$", () -> {
+            driver.close();
+        });
     }
+
+
 }
